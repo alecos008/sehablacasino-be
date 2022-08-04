74cce072dd96b99741585542b6ea3c77fc4ebd8c
Alejandro Cazorla <acazorla72@gmail.com>
2022-08-04 19:19:07 +0200

    route functioning locally

---
+++ models/User.model.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    number: String,
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
