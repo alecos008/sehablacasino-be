34b24d49cdffebd7caac4b95b993f50b2588755c
Alejandro Cazorla <acazorla72@gmail.com>
2022-08-04 19:31:20 +0200

    added default route

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,9 +2,9 @@ const router = require("express").Router();
 const User = require("../models/User.model");
 const registrationRoutes = require("./registration.routes");
 
-/* router.get("/", (req, res, next) => {
+router.get("/", (req, res, next) => {
   res.json("All good in here");
-}); */
+});
 
 router.use("/registration", registrationRoutes);
 
